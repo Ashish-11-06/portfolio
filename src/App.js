f4756d93dbb8099f8accd95a2b7dbf83c0375a63
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-07-27 19:35:47 +0530

    added about

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header'
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+      <div className = "app">
+      <Header />
+        <Home />
+     
+      <Routes>
+        {/* <Route path='/' exact Component={About} /> */}
+        {/* <Route path='/projects' exact Component={Projects} />
+        <Route path='/contact' exact Component={Contact} /> */}
+        {/* <Route path='/resume' exact Component={resume} /> */}
+      </Routes>
+      <About />
+      <Projects />
+      <Contact />
+     
+      </div>
+      <Footer className="footerr"/>
+   </Router>
+   
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../style/About.css'
+
+function About () {
+    return(
+        <div className="About">
+               <h1>About Me</h1>
+               <p>Hi, I'm Ashish Bhosale, a passionate web developer...</p>
+        </div>
+    );
+}
+
+export default About;
