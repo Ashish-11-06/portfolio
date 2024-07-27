7da86b15118bb6c0304bd4a8bb1789c5d4462e8e
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-07-27 19:37:22 +0530

    Footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>&copy; 2024 Ashish Bhosale. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
