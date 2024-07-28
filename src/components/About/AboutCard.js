4e85ef089e56f368bdab81d4953592d9d0ba624e
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-07-28 18:46:59 +0530

    About Compo added

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Ashish Bhosale </span>
+            from <span className="purple"> Pune, India. </span>
+            I have completed MCA (Master Of Computer Application) in SPPU.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Flute and Guitar
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Sketching ballpen and pencil
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
