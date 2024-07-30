f9474ce2017961f699d3cdd02a30d8b2e9644675
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-07-30 18:24:17 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header'
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Home from './components/Home';
+import React, { useState, useEffect } from "react";
+
+// import About from "./components/About/About";
+// import Projects from "./components/Projects/Projects";
+// import Footer from "./components/Footer";
+// import Resume from "./components/Resume/ResumeNew";
+import Preloader from './components/Pre';
+import Navbar from './components/Navbar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume/ResumeNew";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import './style/style.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-   <Router>
-      <div className = "app">
-      <Header />
-        <Home />
-     
-      <Routes>
-        {/* <Route path='/' exact Component={About} /> */}
-        {/* <Route path='/projects' exact Component={Projects} />
-        <Route path='/contact' exact Component={Contact} /> */}
-        {/* <Route path='/resume' exact Component={resume} /> */}
-      </Routes>
-      <About />
-      <Projects />
-      <Contact />
-     
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer />
       </div>
-      <Footer className="footerr"/>
-   </Router>
-   
+    </Router>
   );
 }
 
