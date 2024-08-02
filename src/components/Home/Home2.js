4b363df3fb39d6c5a08942efb50c9c2de8bea021
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-08-02 22:34:53 +0530

    remove warning

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,7 +4,6 @@ import myImg from "../../Assets/avatar.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
