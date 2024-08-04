d494d1d972409429f330715c1fa8d9a1640992af
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-08-04 15:14:39 +0530

    warnings resolve

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
@@ -15,7 +14,6 @@ import {
 } from "react-icons/di";
 import {
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.png";
-import homeBack from "../../Assets/hero-back-illustration.svg";
 import Home2 from "./Home2";
 import Type from "./Type";
-import About from '../About/About'
 
 function Home() {
   return (
